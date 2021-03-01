9c6d7aa482d96dda62e7d8aec7147d41b49cb450
27rabbit <657339595@qq.com>
2021-03-01 19:48:41 +0800

    exp00-2

---
+++ MUX2T1_5/MUX2T1_5.srcs/sim_1/new/MUX2T1_5_TB.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/03/01 19:31:04
+// Design Name: 
+// Module Name: MUX2T1_5_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX2T1_5_TB(
+    );
+    reg s;
+    reg [4:0] I0, I1;
+    wire [4:0] O;
+    MUX2T1_5 m1 (.I0(I0), .I1(I1), .s(s), .O(O));
+    
+    initial begin
+    
+        #100
+        I0 = 4'b1111;
+        I1 = 4'b0000;
+        
+        s = 1'b1;
+        
+        #100
+        
+        s = 1'b0;
+        
+        
+    
+    end
+    
+endmodule
